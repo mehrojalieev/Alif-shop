bf8f5b200d29c409ff5e2ad16d2743eb1c4bb1e1
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-03-11 09:41:34 +0500

    Completed Prayer-times route

--- src/router/PrayerTime.vue
+++ src/router/PrayerTime.vue
@@ -43,44 +43,44 @@ import { RouterLink } from 'vue-router';
                 <div class="item-content">
                     <span class="material-symbols-outlined">location_on</span>
                     <p>Region:</p>
-                    <!-- <h4>{{this.time_data.region }}</h4> -->
+                    <h4>{{ this.time_data && this.time_data.region }}</h4>
                 </div>
                 <div class="item-content">
                     <span class="material-symbols-outlined">date_range</span>
                     <p>Hafta kuni:</p>
-                    <!-- <h4>{{this.time_data.weekday}}</h4> -->
+                    <h4>{{ this.time_data && this.time_data.weekday}}</h4>
                 </div>
                 <div class="item-content">
                     <span class="material-symbols-outlined">schedule</span>
                     <p>Sana:</p>
-                    <!-- <h4>{{this.time_data.date}}</h4> -->
+                    <h4>{{ this.time_data && this.time_data.date}}</h4>
                 </div>
 
             </div>
             <div class="time__card-wrapper">
                 <div class="prayer__time-card">
                         <p>Bomdod</p>
-                        <h3>{{ this.time_data.times.tong_saharlik }}</h3>
+                        <h3>{{ this.time_data && this.time_data?.times?.tong_saharlik }}</h3>
                 </div>
                 <div class="prayer__time-card">
                         <p>Peshin</p>
-                        <h3>{{ this.time_data.times.peshin }}</h3>
+                        <h3>{{ this.time_data && this.time_data?.times?.peshin }}</h3>
                 </div>
                 <div class="prayer__time-card">
                         <p>Asr</p>
-                        <h3>{{ this.time_data.times.asr }}</h3>
+                        <h3>{{ this.time_data && this.time_data.times.asr }}</h3>
                 </div>
                 <div class="prayer__time-card">
                         <p>Shom</p>
-                        <h3>{{ this.time_data.times.shom_iftor }}</h3>
+                        <h3>{{ this.time_data && this.time_data.times.shom_iftor }}</h3>
                 </div>
                 <div class="prayer__time-card">
                         <p>Xufton</p>
-                        <h3>{{ this.time_data.times.hufton }}</h3>
+                        <h3>{{ this.time_data && this.time_data.times.hufton }}</h3>
                 </div>
             </div>
 
-            <RouterLink class="back-home" to="/">
+            <RouterLink class="back__home-link" to="/">
                 <span class="material-symbols-outlined">arrow_back</span>
                 <strong>Xaridga qaytish</strong>
             </RouterLink>
@@ -107,8 +107,8 @@ import { RouterLink } from 'vue-router';
             padding-bottom: 10px;
             select{
                 border-radius: 8px;
-                padding: 7px 14px;
-                font-size: 16px;
+                padding: 6px 10px;
+                font-size: 15px;
                 outline-color: var(--warning-color);
                 border: 1px solid #c4c6c7;
             }
@@ -118,43 +118,363 @@ import { RouterLink } from 'vue-router';
             align-items: center;
             column-gap: 3px;
             span{
-                font-size: 23px;
+                font-size: 19px;
             }
             p{
-                font-size: 18px;
+                font-size: 15px;
                 color: var(--secondary-color);
-                margin-right: 6px;
+                margin-right: 8px;
             }
             h4{
                 font-weight: 500;
-                font-size: 22px;
-                color: var(--warning-color);
+                font-size: 17px;
             }
         }
 
         .time__card-wrapper{
             display: flex;
             align-items: center;
-            justify-content: space-around;
+            column-gap:13px;
+            justify-content: space-between;
             
             width: 100%;
-            margin-top: 10px;
+            margin-top: 20px;
         }
         .prayer__time-card{
             display: grid;
             place-items: center;
-            padding: 1.2rem ;
+            padding: .5rem 2rem ;
             width: 100%;
-            max-width: 164px;
+            max-width: 180px;
             border-radius: 8px;
-            border: 1px solid #E3E8EA;
+            border: 2px solid #E3E8EA;
             background-color: #F4F6F7;
+            &:hover{
+                cursor: pointer;
+                background-color: var(--light-color);
+                border: 2px solid var(--warning-color);
+            }
+            p{
+                font-size: 17px;
+            }
+            h3{
+                font-size: 33px;
+                font-weight: 500;
+            }
+        }
+        .back__home-link{
+            max-width: 420px;
+            height: 52px;
+            background-color: var(--warning-color);
+            text-decoration: none;
+            margin: 80px  auto 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            column-gap: 5px;
+            color: var(--dark-color);
+            border-radius: 8px;
+            transition: .2s;
+            &:hover{
+                cursor: pointer;
+                background-color: var(--warning-hover-color);
+            }
+            span{
+                font-size: 22px;
+            }
+            strong  {
+            font-weight: 500;
+            font-size: 16px;
+
+            }
+
+        }
+
+        @media only screen and (max-width: 978px){
+            .time__content-header{
+            column-gap: 50px;
+            select{
+                padding: 5px 8px;
+                font-size: 14px;
+            }
+        }
+        .item-content{
+            column-gap: 3px;
+            span{
+                font-size: 18px;
+            }
+            p{
+                font-size: 15px;
+                margin-right: 7px;
+            }
+            h4{
+                font-size: 16px;
+            }
+        }
+            .prayer__time-card{
+            padding: .5rem 1.4rem !important;
+            max-width: 170px;
             p{
                 font-size: 16px;
             }
             h3{
-                font-size: 30px;
-                font-weight: 500;
+                font-size: 31px;
             }
+        }
+        }
+
+        @media only screen and (max-width: 850px){
+            .time__content-header{
+            column-gap: 27px;
+            select{
+                padding: 4px 6px;
+                font-size: 14px;
+            }
+        }
+        .item-content{
+            column-gap: 3px;
+            span{
+                font-size: 17px;
+            }
+            p{
+                font-size: 14px;
+                margin-right: 7px;
+            }
+            h4{
+                font-size: 16px;
+            }
+        }
+        .time__card-wrapper{
+            column-gap: 5px;
+        }
+            .prayer__time-card{
+            padding: .3rem 1.2rem !important;
+            max-width: 140px;
+            p{
+                font-size: 15px;
+            }
+            h3{
+                font-size: 26px;
+            }
+        }
+        }
+        @media only screen and (max-width: 770px){
+            .time__content-header{
+            column-gap: 20px;
+            select{
+                padding: 4px 6px;
+                font-size: 14px;
+            }
+        }
+        .item-content{
+            column-gap: 3px;
+            span{
+                font-size: 17px;
+            }
+            p{
+                font-size: 13px;
+                margin-top: 3px;
+                margin-right: 7px;
+            }
+            h4{
+                font-size: 15px;
+            }
+        }
+        .time__card-wrapper{
+            justify-content: space-evenly;
+            column-gap: 5px;
+        }
+            .prayer__time-card{
+            padding: .2rem .8rem !important;
+            max-width: 120px;
+            p{
+                font-size: 14px;
+            }
+            h3{
+                font-size: 24px;
+            }
+        }
+        }
+
+        @media only screen and (max-width: 713px){
+            .time__content-header{
+            column-gap: 20px;
+            select{
+                font-size: 14px;
+            }
+        }
+        .item-content{
+            column-gap: 3px;
+            span{
+                font-size: 21px;
+            }
+            p{
+                display: none;
+            }
+            h4{
+                margin-top: 2px;
+                font-size: 17px;
+            }
+        }
+        .time__card-wrapper{
+            width: 100%;
+            display: block;
+            column-gap: 5px;
+        }
+            .prayer__time-card{
+            margin-bottom: 1rem;
+            justify-content: flex-start;
+            padding: 1rem .8rem !important;
+            max-width: 650px;
+            p{
+                font-size: 17px;
+            }
+            h3{
+                font-size: 28px;
+            }
+        }
+        }
+        @media only screen and (max-width: 588px){
+            .time__content-header{
+            column-gap: 20px;
+            select{
+                font-size: 13px;
+                padding: 5px 1px;
+            }
+        }
+        .item-content{
+            column-gap: 3px;
+            span{
+                font-size: 17px;
+            }
+            h4{
+                margin-top: 2px;
+                font-size: 15px;
+            }
+        }
+        .time__card-wrapper{
+            width: 100%;
+            column-gap: 5px;
+        }
+            .prayer__time-card{
+            padding: .7rem .7rem !important;
+            max-width: 550px;
+            p{
+                font-size: 16px;
+            }
+            h3{
+                font-size: 28px;
+            }
+        }
+        }
+
+        @media only screen and (max-width: 530px){
+            .time__content-header{
+            select{
+                width: 100%;
+                font-size: 16px;
+                padding: .5rem;
+            }
+        }
+        .item-content{
+            display: none;
+        }
+        .time__card-wrapper{
+            width: 100%;
+            column-gap: 5px;
+        }
+            .prayer__time-card{
+            padding: .7rem .7rem !important;
+            max-width: 550px;
+            p{
+                font-size: 16px;
+            }
+            h3{
+                font-size: 28px;
+            }
+        }
+        }
+        @media only screen and (max-width: 440px){
+            .time__content-header{
+            select{
+                width: 100%;
+                font-size: 15px;
+                padding: .4rem;
+            }
+        }
+
+        .time__card-wrapper{
+            width: 100%;
+        }
+            .prayer__time-card{
+            padding: .5rem !important;
+            max-width: 550px;
+            p{
+                font-size: 15px;
+            }
+            h3{
+                font-size: 26px;
+            }
+        }
+        }
+        @media only screen and (max-width: 320px){
+            .time__content-header{
+            select{
+                font-size: 14px;
+                padding: .3rem;
+            }
+        }
+
+        .time__card-wrapper{
+            margin-top: 15px;
+            width: 100%;
+        }
+            .prayer__time-card{
+            padding: .5rem !important;
+            max-width: 550px;
+            p{
+                font-size: 14px;
+            }
+            h3{
+                font-size: 24px;
+            }
+        }
+        .back__home-link{
+            margin-top: 30px;
+            height: 38px;
+            span{
+                font-size: 20px;
+            }
+            strong{
+                font-size: 15px;
+            }
+        }
+        }
+        @media only screen and (max-width: 266px){
+            .prayer__time-title{
+                font-size: 22px;
+            }
+        .time__card-wrapper{
+            margin-top: 15px;
+        }
+            .prayer__time-card{
+            padding: .3rem .5rem !important;
+            p{
+                font-size: 13px;
+            }
+            h3{
+                font-size: 22px;
+            }
+        }
+        .back__home-link{
+            margin-top: 30px;
+            height: 38px;
+            span{
+                font-size: 18px;
+            }
+            strong{
+                font-size: 14px;
+            }
+        }
         }
 </style>
