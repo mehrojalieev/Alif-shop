ef4afc93278303a53e21a110806398ed9b9254d6
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-03-22 10:53:23 +0500

    I completed footer component

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import {RouterView} from 'vue-router'
 import Home from "@/views/Home.vue"
 import NavbarTop from '@/components/Navbar-top.vue'
         import Nav from '@/layout/Nav.vue';
+        import Footer from '@/components/Footer.vue'
 export default {
-  components: {Home, NavbarTop, Nav}
+  components: {Home, NavbarTop, Nav,Footer}
 }
 </script>
 
@@ -13,6 +14,7 @@ export default {
     <NavbarTop/>
     <Nav/>
     <RouterView/>
+    <Footer/>
   </div>
 </template>
 
